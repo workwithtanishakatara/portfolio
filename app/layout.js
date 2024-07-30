513aa68e1fdc81d75f96e026e9261b437bf86b40
WaleedNasirAlsiraj <waleednasiralsiraj@gmail.com>
2024-07-30 18:34:50 +0500

    added the google analystics scripts

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Footer from "@/components/footer/Footer";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/ProgressBar";
 import { fetchData } from "@/lib/FetchData";
+import Script from "next/script";
 
 const inter = Poppins({
   subsets: ["latin"],
@@ -17,15 +18,29 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-
-  const navbarData = await fetchData("https://9wlw9jiw.api.sanity.io/v2022-03-07/data/query/production?query=%7B%0A++%22about%22%3A+*%5B_type+%3D%3D+%22aboutPage%22%5D%7Btitle%2C+description%7D%5B0%5D%2C%0A++%22speaking%22%3A+*%5B_type+%3D%3D+%22speakPage%22%5D%7Btitle%2C+description%7D%5B0%5D%2C%0A++%22writing%22%3A+*%5B_type+%3D%3D+%22writingPage%22%5D%7Btitle%2C+description%7D%5B0%5D%2C%0A++++%22socialLinks%22%3A+*%5B_type+%3D%3D+%22homePage%22%5D%7BsocialLinks%7D%5B0%5D%0A%7D")
+  const navbarData = await fetchData(
+    "https://9wlw9jiw.api.sanity.io/v2022-03-07/data/query/production?query=%7B%0A++%22about%22%3A+*%5B_type+%3D%3D+%22aboutPage%22%5D%7Btitle%2C+description%7D%5B0%5D%2C%0A++%22speaking%22%3A+*%5B_type+%3D%3D+%22speakPage%22%5D%7Btitle%2C+description%7D%5B0%5D%2C%0A++%22writing%22%3A+*%5B_type+%3D%3D+%22writingPage%22%5D%7Btitle%2C+description%7D%5B0%5D%2C%0A++++%22socialLinks%22%3A+*%5B_type+%3D%3D+%22homePage%22%5D%7BsocialLinks%7D%5B0%5D%0A%7D"
+  );
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-N4KEZFQLVW"
+        />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N4KEZFQLVW');`}
+        </Script>
+      </head>
       <body className={cn(inter.className, "!tracking-tight")}>
-        <Navbar data={navbarData}/>
+        <Navbar data={navbarData} />
         <Providers>{children}</Providers>
-        <Footer data={navbarData}/>
+        <Footer data={navbarData} />
       </body>
     </html>
   );
