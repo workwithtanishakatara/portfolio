cf56bfe1dcfdfa179de08723c5c62359d80cb06d
WaleedNasirAlsiraj <waleednasiralsiraj@gmail.com>
2024-08-02 02:40:38 +0500

    optimized

--- sanityConfig.js
+++ sanityConfig.js
@@ -8,5 +8,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion, // https://www.sanity.io/docs/api-versioning
-  useCdn: false, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+  useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 })
