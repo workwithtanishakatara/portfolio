6bfc74a42fc2e4f004bb3ac8a2b244eebd2c3427
WaleedNasirAlsiraj <waleednasiralsiraj@gmail.com>
2024-08-01 13:55:17 +0500

    fixes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -30,46 +30,35 @@ const page = async () => {
           <div className="relative w-full mt-10">
             <Marquee pauseOnHover className={`[--duration:40s]`}>
               {data.testimonials.map((review, index) => (
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <a
-                        target="_blank"
-                        href={review.link}
-                        key={index}
-                        className="w-[350px] cursor-pointer h-full justify-between flex flex-col gap-12 group p-5 hover:scale-105 transition-all duration-500 bg-slate-100 rounded-2xl"
-                      >
-                        <p className="text-sm text-left group-hover:text-neutral-800 transition-all duration-500 text-neutral-600 tracking-tight">
-                          {review.review}
-                        </p>
-                        <div className="flex w-full flex-row items-center justify-between">
-                          <div className="w-full flex flex-row gap-3 items-center justify-start">
-                            <img
-                              width={40}
-                              height={40}
-                              className="rounded-full"
-                              src={urlFor(review.image).url()}
-                            />
-                            <div>
-                              <p className="text-sm text-left text-neutral-600 tracking-tight">
-                                {review.name}
-                              </p>
-                              <p className="text-sm text-left text-neutral-600 tracking-tight">
-                                {review.description}
-                              </p>
-                            </div>
-                          </div>
-                          <MdArrowOutward />
-                        </div>
-                      </a>
-                    </TooltipTrigger>
-                    <TooltipContent>
+                <a
+                  target="_blank"
+                  href={review.link}
+                  key={index}
+                  className="w-[350px] cursor-pointer h-full justify-between flex flex-col gap-12 group p-5 hover:scale-105 transition-all duration-500 bg-slate-100 rounded-2xl"
+                >
+                  <p className="text-sm text-left group-hover:text-neutral-800 transition-all duration-500 text-neutral-600 tracking-tight">
+                    {review.review}
+                  </p>
+                  <div className="flex w-full flex-row items-center justify-between">
+                    <div className="w-full flex flex-row gap-3 items-center justify-start">
+                      <img
+                        width={40}
+                        height={40}
+                        className="rounded-full"
+                        src={urlFor(review.image).url()}
+                      />
                       <div>
-                        Click To Visit
+                        <p className="text-sm text-left text-neutral-600 tracking-tight">
+                          {review.name}
+                        </p>
+                        <p className="text-sm text-left text-neutral-600 tracking-tight">
+                          {review.description}
+                        </p>
                       </div>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
+                    </div>
+                    <MdArrowOutward />
+                  </div>
+                </a>
               ))}
             </Marquee>
             <div className="pointer-events-none opacity-0 md:opacity-100 absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
