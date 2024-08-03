21e31660b37ace683530cc9071bc18e1f9a3caf6
WaleedNasirAlsiraj <waleednasiralsiraj@gmail.com>
2024-08-03 13:25:21 +0500

    changes

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -81,6 +81,7 @@ export async function POST(request) {
       html: `
             <p>Name: ${name} </p>
             <p>Email: ${email} </p>
+            <p>Email: ${company} </p>
             <p>Message: ${message} </p>
             `,
     });
