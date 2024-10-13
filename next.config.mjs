f6fc842350c3ce8d81e5e9d252bb40de39bbdcb6
WaleedNasirAlsiraj <waleednasiralsiraj@gmail.com>
2024-10-13 22:09:41 +0500

    fix google issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com', "api.microlink.io", "cdn.sanity.io", "scontent.cdninstagram.com"], // Add your desired domain(s)
-       
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "api.microlink.io",
+      "cdn.sanity.io",
+      "scontent.cdninstagram.com",
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/?trk=:path*",
+        destination: "/",
+        permanent: true, // This sets it as a 301 redirect
       },
+    ];
+  },
 };
 
 export default nextConfig;
